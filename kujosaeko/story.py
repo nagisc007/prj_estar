90e6d00adf30c624cc7b22d38a489b94ca06ac12
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-03-24 14:16:09 +0900

    [WIP] implement #13: episode tests [ci skip] (#16)
    * [WIP] implement: episode tests [ci skip]
    
    * Updated: submodule - negative mode
    
    * Added: episode outlines
    
    resolved #13

--- kujosaeko/story.py
+++ kujosaeko/story.py
@@ -27,86 +27,68 @@ class Misa(Person):
     def __init__(self):
         super().__init__("古峰未沙", 20, "female", "専門学校生", "わたし", "正人のバイト先の女性")
 
-class KujoDad(Person):
-    def __init__(self):
-        super().__init__("九条父", 44, "male", "強盗", "オレ", "冴子の実父。小さい頃に離婚している")
+def kujo_dad():
+    return Person("九条父", 44, "male", "強盗", "オレ", "冴子の実父。小さい頃に離婚している")
 
 
 # stages
-class MasatoApart(Stage):
-    def __init__(self):
-        super().__init__("正人のアパート", "正人が住むアパート")
+def masato_apart():
+    return Stage("正人のアパート", "正人が住むアパート")
 
+def univ_hakata():
+    return Stage("博多大学", "架空の大学。福岡にある私立大学。")
 
-class UnivHakata(Stage):
-    def __init__(self):
-        super().__init__("博多大学", "架空の大学。福岡にある私立大学。")
+def west_park():
+    return Stage("西公園", "実在する公園。福岡中央区の海べりにあり、桜の名所。")
 
-class WestPark(Stage):
-    def __init__(self):
-        super().__init__("西公園", "実在する公園。福岡中央区の海べりにあり、桜の名所。")
+def misa_manshion():
+    return Stage("未沙のマンション")
+
+def kujo_apart():
+    return Stage("九条のアパート")
+
+def tutor_center():
+    return Stage("家庭教師事務所")
 
 
 # items
-class Gun(Item):
-    def __init__(self):
-        super().__init__("拳銃", "重要なアイテム")
+def dad_gun():
+    return Item("拳銃", note="冴子が隠し持っていた父の形見")
 
-class ToyBox(Item):
-    def __init__(self):
-        super().__init__("おもちゃ箱", "九条冴子が大事にしている玩具の金庫")
+def toy_box():
+    return Item("玩具箱", "九条冴子が大事にしている玩具の金庫")
 
 
 # words
-class Blagger(Word):
-    def __init__(self):
-        super().__init__("強盗事件")
+def blagger():
+    return Word("強盗事件")
 
 
 # daytimes
-class WhiteDay(DayTime):
-    def __init__(self):
-        super().__init__("ホワイトディ", 3, 14, 2019)
+def white_day():
+    return DayTime("ホワイトディ", 3, 14, 2019)
 
-class IncidentDay(DayTime):
-    def __init__(self):
-        super().__init__("事件日", 3, 23, 2019, note="事件の起こる日")
+def parted_day():
+    return DayTime("別々の日", 3, 15, 2019, note="数日間、二人は別れたまま")
+
+def incident_day():
+    return DayTime("事件日", 3, 23, 2019)
 
 
 # episodes
-def ep_intro(ma: Master):
+def ep_intro(ma: Master,
+        masato: Masato, kujo: Kujo,
+        park: Stage,
+        day: DayTime):
     return ma.story(
-            )
-
-
-def ep_relationship(ma: Master):
-    return ma.story(
-            )
-
-
-def ep_incident(ma: Master):
-    return ma.story(
-            )
-
-# story
-def story() -> ActionGroup:
-    '''Define main story.
-    '''
-    ma = Master("my story")
-    kujo, masato, misa, dad = Kujo(), Masato(), Misa(), KujoDad()
-    apart, univi, park = MasatoApart(), UnivHakata(), WestPark()
-    gun = Gun()
-    blagger = Blagger()
-    wday, iday = WhiteDay(), IncidentDay()
-
-    return ma.story(
-            ma.title("九条冴子は返さない"),
-            ep_intro(ma),
-            ep_relationship(ma),
-            ep_incident(ma),
+            ma.title("彼女は返さない"),
+            ma.comment("九条冴子の特殊性と、二人の関係性の描写"),
             masato.be("付き合っている", kujo),
+            masato.want("お返し", kujo),
+            masato.give("チョコ", kujo),
+            masato.invite("デート", kujo),
             kujo.think("何も返さない"),
-            wday.explain("薄曇りの日の午後"),
+            day.explain("薄曇りの日の午後"),
             park.explain("公園の桜は雨に打たれて散々"),
             masato.want("何か返して", kujo),
             kujo.give("ない", masato),
@@ -114,16 +96,87 @@ def story() -> ActionGroup:
             kujo.talk("誰か別の人に返せばいつか大きくなって戻ってくる"),
             masato.tell("じゃあ俺には？"),
             kujo.tell("返さない"),
+            masato.give("サンドウィッチ", kujo),
+            kujo.returns("もらったサンドウィッチ", masato).negative(),
             masato.talk("口論", kujo),
-            masato.go("他の女"),
-            iday.explain("雨の冷たい日"),
-            blagger.explain("ニュースで事件報道"),
+            )
+
+
+def ep_relationship(ma: Master,
+        masato: Masato, kujo: Kujo, misa: Misa,
+        toybox: Item, gun: Item,
+        kapart: Stage, mapart: Stage, manshion: Stage, tutor: Stage,
+        day: DayTime):
+    return ma.story(
+            ma.title("離れた心は取り戻せない"),
+            ma.comment("二人の関係の決裂と、冴子父の事件の伏線"),
+            day.explain("後日"),
+            tutor.explain("バイト先の家庭教師派遣事務所"),
+            masato.meet("", misa),
+            misa.ask("関係", kujo),
+            misa.talk("彼女の噂", kujo),
+            masato.want("彼女の考え", kujo),
+            masato.think("事情がある", kujo),
+            masato.visit("", kapart),
+            masato.ask("", toybox),
+            masato.open("", toybox),
+            kujo.angry("触るな", toybox).set_flag(gun.name),
+            masato.talk("口論", kujo),
+            kujo.talk("他の女と付き合えばいい", masato),
+            misa.phone("", masato),
+            misa.invite("デート", masato),
+            masato.accept("彼女の申し出", misa),
+            masato.go("他の女", misa),
+            masato.visit("", manshion),
+            masato.love("", misa),
+            )
+
+
+def ep_incident(ma: Master,
+        masato: Masato, kujo: Kujo, misa: Misa, dad: Person,
+        gun: Item, blag: Word,
+        mapart: Stage, kapart: Stage,
+        day: DayTime):
+    return ma.story(
+            ma.title("その絆は離せない"),
+            ma.comment("事件と彼女が与えたもの"),
+            day.explain("雨の冷たい日"),
+            mapart.explain("湿気った自分の部屋"),
+            masato.watch("ぼんやりとTVを"),
+            blag.explain("ニュースで事件報道"),
+            masato.know("", blag),
+            masato.contact("", kujo),
+            masato.want("助ける", kujo),
             masato.find("彼女の部屋",gun).set_deflag(gun.name),
             kujo.talk("父親の話",masato),
             kujo.tell("いつもいっぱいあげてた", masato),
             dad.shoot("", kujo),
             masato.guard("", kujo),
             dad.catch("警察").passive(),
+            )
+
+# story
+def story() -> ActionGroup:
+    '''Define main story.
+    '''
+    ma = Master("my story")
+    kujo, masato, misa, dad = Kujo(), Masato(), Misa(), kujo_dad()
+    mapart, kapart, manshion = masato_apart(), kujo_apart(), misa_manshion()
+    park, tutor = west_park(), tutor_center()
+    gun, toybox = dad_gun(), toy_box()
+    blag = blagger()
+    wday, pday, iday = white_day(), parted_day(), incident_day()
+
+    return ma.story(
+            ma.title("九条冴子は返さない"),
+            ep_intro(ma, masato, kujo,
+                park, wday),
+            ep_relationship(ma, masato, kujo, misa,
+                toybox, gun,
+                kapart, mapart, manshion, tutor, pday),
+            ep_incident(ma, masato, kujo, misa, dad,
+                gun, blag,
+                mapart, kapart, iday),
             )
 
 
--- tests/test_kujosaeko.py
+++ tests/test_kujosaeko.py
@@ -10,7 +10,11 @@ import storybuilder.builder.testtools as testtools
 
 from kujosaeko.story import story
 from kujosaeko.story import ep_incident, ep_intro, ep_relationship
-from kujosaeko.story import Kujo, Masato
+from kujosaeko.story import Kujo, Masato, Misa
+from kujosaeko.story import kujo_dad
+from kujosaeko.story import masato_apart, kujo_apart, misa_manshion, west_park, tutor_center
+from kujosaeko.story import toy_box, dad_gun, blagger
+from kujosaeko.story import incident_day, parted_day, white_day
 
 
 class StoryTest(unittest.TestCase):
@@ -32,8 +36,8 @@ class StoryTest(unittest.TestCase):
     def test_has_outline_infos(self):
         self.assertTrue(testtools.has_outline_infos(self, self.story,
             Masato().want("何か返して", Kujo()),
-            Kujo().give("ない", Masato()),
-            Masato().go("他の女"),
+            Kujo().give("", Masato()).negative(),
+            Masato().go("他の女", Misa()),
             Masato().guard("", Kujo())))
 
     def test_followed_flags(self):
@@ -43,13 +47,60 @@ class StoryTest(unittest.TestCase):
 class EpisodeTest(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.ma = Master("test")
+        self.ep1 = ep_intro(self.ma,
+                Masato(), Kujo(),
+                west_park(), white_day())
+        self.ep2 = ep_relationship(self.ma,
+                Masato(), Kujo(), Misa(),
+                toy_box(), dad_gun(),
+                kujo_apart(), masato_apart(), misa_manshion(), tutor_center(), parted_day())
+        self.ep3 = ep_incident(self.ma,
+                Masato(), Kujo(), Misa(), kujo_dad(),
+                dad_gun(), blagger(),
+                masato_apart(), kujo_apart(), incident_day())
 
     def test_ep1_is_all_actions(self):
-        pass
+        self.assertTrue(testtools.is_all_actions(self, self.ep1))
+
+    def test_ep2_is_all_actions(self):
+        self.assertTrue(testtools.is_all_actions(self, self.ep2))
+
+    def test_ep3_is_all_actions(self):
+        self.assertTrue(testtools.is_all_actions(self, self.ep3))
 
     def test_ep1_has_basic_infos(self):
-        pass
+        self.assertTrue(testtools.has_basic_infos(self, self.ep1,
+            Masato(), Kujo()))
+
+    def test_ep2_has_basic_infos(self):
+        self.assertTrue(testtools.has_basic_infos(self, self.ep2,
+            Masato(), Kujo()))
+
+    def test_ep3_has_basic_infos(self):
+        self.assertTrue(testtools.has_basic_infos(self, self.ep3,
+            Masato(), Kujo()))
 
     def test_ep1_has_outline_infos(self):
-        pass
+        self.assertTrue(testtools.has_outline_infos(self, self.ep1,
+            Masato().want("お返し", Kujo()),
+            Masato().give("チョコ", Kujo()),
+            Masato().invite("デート", Kujo()),
+            Kujo().returns("もらったサンドウィッチ", Masato()).negative(),
+            ))
+
+    def test_ep2_has_outline_infos(self):
+        self.assertTrue(testtools.has_outline_infos(self, self.ep2,
+            Masato().want("彼女の考え", Kujo()),
+            Masato().think("事情がある", Kujo()),
+            Masato().open("", toy_box()),
+            Masato().go("他の女", Misa()),
+            ))
+
+    def test_ep3_has_outline_infos(self):
+        self.assertTrue(testtools.has_outline_infos(self, self.ep3,
+            Masato().want("助ける", Kujo()),
+            Masato().know("", blagger()),
+            Masato().contact("", Kujo()),
+            Masato().guard("", Kujo()),
+            ))
