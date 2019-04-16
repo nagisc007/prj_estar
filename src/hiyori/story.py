e3694858d930e8347d4a580cdc093b4da36fdc44
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-04-16 15:51:30 +0900

    [WIP] Added #45: ep2 sc1 (#50)
    * [WIP] Added: ep2 sc1
    
    * Added: looking test
    
    * Added: ep2 scenes
    
    * [WIP] Added: ep3
    
    * Fixed: typo (forgot master)
    
    * [WIP] Added: ep2
    
    * Added: scene ep4
    
    resolved #45

--- src/hiyori/story.py
+++ src/hiyori/story.py
@@ -16,10 +16,15 @@ CHARAS = (
         ("yuko", "浅見夕子", 18, "female", "高校生"),
         ("tagawa", "田川昼男", 18, "male", "高校生"),
         ("takemura", "竹村紀行", 56, "male", "数学教師"),
+        ("boys", "男子生徒", 18, "male", "高校生"),
+        ("eri", "足立衣里", 18, "female", "高校生"),
+        ("granpa", "浅見健三", 78, "male", "農家", "夕子の祖父"),
+        ("mam", "浅見晶子", 40, "female", "レジ打ち", "夕子の母"),
         )
 
 STAGES = (
-        ("school", "高校"),
+        ("town", "衣保川町", "地方都市（岐阜県・揖斐川がモデル）の田舎町"),
+        ("school", "高校", "県立高校"),
         ("classroom", "教室"),
         ("tanbo", "田んぼ"),
         ("tahouse", "田川家"),
@@ -35,89 +40,199 @@ DAYS = (
 
 ITEMS = (
         ("tractor", "トラクター"),
+        ("cycle", "自転車", "夕子が通学に利用する自転車"),
+        ("uniform", "学生服"),
+        ("futurepaper", "進路予定表"),
         )
 
 WORDS = (
         ("hiyori", "田川日和"),
         ("myhiyori", "自分日和"),
         ("reason", "サボる理由"),
+        ("fam_reason", "家族の事情", "祖父が昨年秋に腰をやって入退院繰り返す"),
         ("circs", "田川の事情"),
         ("myfuture", "自分の将来"),
         )
 
+FLAGS = (
+        ("hiyori", "田川日和の謎"),
+        ("future", "夕子の将来"),
+        )
 
 # episodes
 def ep_intro(ma: Master):
-    yuko, tagawa = ma.yuko, ma.tagawa
+    yuko, tagawa, boys, eri, takemura = ma.yuko, ma.tagawa, ma.boys, ma.eri, ma.takemura
     sunnyday = ma.sunnyday
-    classroom = ma.classroom
-    tanbo = ma.tanbo
-    reason = ma.reason
-    return ma.story("田川日和",
+    nextday = sunnyday.elapsed_day(day=1)
+    classroom, school, town = ma.classroom, ma.school, ma.town
+    reason, hiyori = ma.reason, ma.hiyori
+    tractor, uniform, futurepaper = ma.tractor, ma.uniform, ma.futurepaper
+    sc1 = ma.scene("田川のいない日",
+            ma.comment("夕子の一人称"),
+            ma.comment("田川はep2まで登場させない"),
+            classroom.explain("教室の様子"),
             yuko.be(classroom, sunnyday),
-            yuko.look(tagawa, "サボる"),
-            yuko.go(tagawa, tanbo),
-            yuko.talk(tagawa, "将来"),
+            sunnyday.explain("午前の休み時間"),
+            yuko.explain("同じ制服に同じようなもっさり", uniform),
+            yuko.have(futurepaper, ma.f_future),
+            boys.talk(tagawa, "またサボり"),
+            boys.explain(uniform, "制服の前を全開にした男子"),
+            yuko.hear(eri, reason),
+            eri.reply(yuko, "知らない"),
+            eri.explain("長い黒髪が暑そう"),
+            eri.look("読書"),
+            eri.talk(yuko, reason, "今年は多い"),
             yuko.know(tagawa, reason).want(),
-            sunnyday.explain("翌日もよく晴れた"),
-            tagawa.be(classroom).non(),
-            yuko.think("サボる決断"),
-            yuko.go("学校を抜け出す"),
+            yuko.look(sunnyday, "驚くほどの晴天"),
+            town.explain("見渡す限り閑散とした町並み"),
+            takemura.come(classroom, "授業の鐘"),
+            takemura.explain("担任", "定年間際で白髪混じり"),
             )
+    sc2 = ma.scene("抜け出す夕子",
+            nextday.explain("翌日も晴れ"),
+            tagawa.be(classroom).non(),
+            yuko.look(tagawa, "サボる"),
+            boys.talk(hiyori, "笑う", ma.f_hiyori),
+            yuko.think("サボる決断"),
+            yuko.talk(eri, "田川を探す"),
+            yuko.go(school, "抜け出す"),
+            school.explain("授業中の学校は静か"),
+            )
+    return ma.story("田川日和", sc1, sc2)
 
 def ep_tagawa(ma: Master):
-    yuko, tagawa = ma.yuko, ma.tagawa
-    sunnyday = ma.sunnyday
-    tanbo = ma.tanbo
-    reason = ma.reason
-    return ma.story("学校外の田川",
+    yuko, tagawa, granpa = ma.yuko, ma.tagawa, ma.granpa
+    nextday = ma.sunnyday.elapsed_day(day=1)
+    tanbo, town = ma.tanbo, ma.town
+    reason, fam_reason, myfuture = ma.reason, ma.fam_reason, ma.myfuture
+    cycle, tractor = ma.cycle, ma.tractor
+    sc1 = ma.scene("田川の居場所",
+            yuko.go(cycle, town),
+            town.explain("田畑が多い", "のどか"),
             yuko.look(tagawa, "どこにいるか"),
             yuko.know(tagawa, reason).want(),
-            yuko.go(tagawa, "尾行"),
-            yuko.look(tagawa, tanbo, sunnyday),
-            yuko.talk(tagawa, reason),
-            tagawa.reply(yuko, reason),
-            yuko.know(tagawa, reason),
+            yuko.look(granpa, tractor),
+            granpa.explain("目元がぼさぼさの毛で覆われ背中が曲がる"),
+            granpa.ask(yuko, "もう学校終わったのか"),
+            yuko.ask(granpa, tagawa, "見なかったか"),
+            granpa.reply(yuko, tagawa, tanbo),
+            yuko.ask(granpa, tagawa, "知ってるの？"),
+            granpa.talk("田川は小さい頃からの喧嘩仲間"),
+            yuko.hear(granpa, tagawa, "居場所"),
             )
+    sc2 = ma.scene("田川の秘密",
+            yuko.go(tanbo),
+            nextday.explain("日が高くなる"),
+            yuko.look(tagawa, tanbo),
+            tanbo.explain("土が耕された広大な区画"),
+            tractor.explain("真っ赤なボディは既に泥まみれ"),
+            tagawa.explain("頭に手ぬぐいをまきつけ、野良作業着"),
+            yuko.talk(tagawa),
+            yuko.ask(tagawa, reason),
+            tagawa.reply(yuko, "終わるまで待ってくれ"),
+            yuko.be("待つ"),
+            yuko.look(tagawa, "普段見せない顔"),
+            )
+    sc3 = ma.scene("田川日和",
+            tagawa.do("農作業終わる", nextday),
+            yuko.have(tagawa, "ジュース"),
+            tagawa.reply(yuko, "ありがとう"),
+            yuko.feel(tagawa, ma.some()),
+            yuko.ask(tagawa, reason),
+            tagawa.reply(yuko, reason).de(ma.f_hiyori),
+            tagawa.talk(yuko, reason, "農業を継ぐ"),
+            tagawa.talk(yuko, "家族の事情", fam_reason),
+            yuko.know(tagawa, reason),
+            tagawa.talk(yuko, "将来農家やりたい"),
+            tagawa.ask(yuko, myfuture),
+            yuko.talk(tagawa, myfuture).non(),
+            )
+    return ma.story("学校外の田川", sc1, sc2, sc3)
 
 def ep_future(ma: Master):
-    yuko, tagawa, takemura = ma.yuko, ma.tagawa, ma.takemura
-    classroom = ma.classroom
+    yuko, tagawa, takemura, eri = ma.yuko, ma.tagawa, ma.takemura, ma.eri
+    classroom, myhouse, school, tanbo = ma.classroom, ma.myhouse, ma.school, ma.tanbo
     rainyday, examday = ma.rainyday, ma.examday
-    myfuture, reason = ma.myfuture, ma.reason
-    return ma.story("自分の将来",
-            yuko.hear(tagawa, reason, rainyday),
+    myfuture, reason, hiyori, myhiyori = ma.myfuture, ma.reason, ma.hiyori, ma.myhiyori
+    cycle, futurepaper = ma.cycle, ma.futurepaper
+    sc1 = ma.scene("夕子の将来",
+            yuko.be(myhouse, rainyday),
+            yuko.hear(tagawa, reason),
+            yuko.think(myfuture).must(),
+            yuko.think(myfuture).non(),
+            )
+    sc2 = ma.scene("試験の日",
+            yuko.go(school, examday),
             tagawa.be(classroom, examday).non(),
+            yuko.ask(eri, tagawa,"いない"),
+            eri.reply(yuko, hiyori),
+            yuko.talk(eri, "馬鹿じゃないの"),
             examday.explain("試験が終わる"),
-            yuko.look(tagawa),
-            yuko.talk(tagawa, "試験をサボったことを咎める"),
+            tagawa.come(school).non(),
+            yuko.go(tanbo, cycle),
+            yuko.look(tagawa, tanbo),
+            yuko.ask(tagawa, "試験をサボったことを咎める"),
+            tagawa.reply(yuko, hiyori),
+            )
+    sc3 = ma.scene("学業と仕事",
+            yuko.talk(tagawa, "将来"),
             tagawa.talk(yuko, "将来に試験が大事か"),
             yuko.reply(tagawa, "大事と思う"),
-            yuko.talk(takemura, "相談"),
+            tagawa.talk(yuko, "大事と思わない"),
+            tagawa.talk(yuko, "日和の方が大事だ"),
+            tagawa.ask(yuko, myhiyori),
+            yuko.reply(tagawa).non(),
+            yuko.be(myhouse, examday.elapsed_day(hour=20)),
             yuko.think(myfuture),
+            yuko.talk("わかんない"),
+            yuko.have(futurepaper, "お嫁さんを消す"),
             )
+    return ma.story("自分の将来", sc1, sc2, sc3)
 
 def ep_myhiyori(ma: Master):
-    yuko, tagawa = ma.yuko, ma.tagawa
+    yuko, tagawa, eri, granpa, mam = ma.yuko, ma.tagawa, ma.eri, ma.granpa, ma.mam
     gwday = ma.gwday
     tahouse, myhouse = ma.tahouse, ma.myhouse
     myhiyori, myfuture = ma.myhiyori, ma.myfuture
-    return ma.story("自分日和",
+    futurepaper = ma.futurepaper
+    sc1 = ma.scene("答えは見えない",
             yuko.be(myhouse, gwday),
+            gwday.explain("どこにも出かけないGW"),
+            myhouse.explain("参考書が並ぶ本棚", "布団をかけたベランダ"),
+            yuko.hear(mam, "ご飯よ"),
+            yuko.go(myhouse, "食堂"),
+            mam.explain("白髪混じりの髪", "ぼさぼさ頭", "染みが多い"),
+            yuko.deal("食事"),
+            yuko.ask(granpa, "仕事のこと"),
+            granpa.reply("そんなもん考えなかった"),
             yuko.think(myfuture),
             yuko.look(myfuture).want(),
-            yuko.go(tahouse, gwday),
-            tagawa.talk(yuko, "自分の日和を持てよ"),
-            yuko.talk(tagawa, myhiyori),
-            yuko.talk(tagawa, "告白"),
-            yuko.look(myhiyori),
             )
+    sc2 = ma.scene("告白と自分日和",
+            yuko.go(tahouse, gwday.elapsed_day(day=1)),
+            tahouse.explain("大きな古い木造平屋"),
+            yuko.go("田川の部屋"),
+            tagawa.ask(yuko, "何の用？"),
+            yuko.reply(tagawa, "この前の話", myhiyori),
+            tagawa.talk(yuko, "自分の日和を持てよ"),
+            yuko.talk(tagawa, myhiyori, "自分の考え"),
+            yuko.talk(tagawa, "告白"),
+            yuko.have(tagawa, futurepaper),
+            futurepaper.be("十年後に結婚日和がみたい"),
+            tagawa.talk(yuko, "馬鹿なのか？"),
+            yuko.reply(tagawa, "十年あったら何でもできるかもよ"),
+            tagawa.look(yuko, "笑顔"),
+            yuko.look(myhiyori),
+            futurepaper.be("看護師と書かれている").de(ma.f_future),
+            )
+    return ma.story("自分日和", sc1, sc2)
 
 
 # main
 def master():
     ma = Master("project hiyori")
     ma.set_db(CHARAS, STAGES, DAYS, ITEMS, WORDS)
+    ma.set_flags(FLAGS)
     return ma
 
 def story(ma: Master):
@@ -129,7 +244,7 @@ def story(ma: Master):
             )
 
 def main(): # pragma: no cover
-    return build_to_story(story())
+    return build_to_story(story(master()))
 
 
 if __name__ == '__main__':
--- tests/test_hiyori.py
+++ tests/test_hiyori.py
@@ -4,8 +4,8 @@
 import unittest
 import storybuilder.builder.testtools as testtools
 from storybuilder.builder.sbutils import print_test_title
-from src.hiyori.story import Something
 from src.hiyori.story import master, story, ep_intro, ep_tagawa, ep_future, ep_myhiyori
+from src.hiyori.story import CHARAS, STAGES
 
 
 _FILENAME = "hiyori.story.py"
@@ -19,21 +19,30 @@ class StoryTest(unittest.TestCase):
 
     def setUp(self):
         self.ma = master()
-        self.st = story(self.ma)
+        self.story = story(self.ma)
         self.ep1 = ep_intro(self.ma)
         self.ep2 = ep_tagawa(self.ma)
         self.ep3 = ep_future(self.ma)
         self.ep4 = ep_myhiyori(self.ma)
 
     def test_is_all_actions(self):
-        self.assertTrue(testtools.is_all_actions(self.st))
+        self.assertTrue(testtools.is_all_actions(self.story))
+
+    @unittest.skip('use each current')
+    def test_exists_looking(self):
+        self.assertTrue(testtools.exists_looking_from_master(self.story, self.ma))
+
+    def test_exists_looking_each(self):
+        for key in [v[0] for v in CHARAS] + [v[0] for v in STAGES]:
+            with self.subTest(key=key):
+                self.assertEqual(testtools.exists_looking_of_the_subject(self.story, self.ma[key]), True)
 
     def test_followed_flags(self):
-        self.assertTrue(testtools.followed_all_flags(self, self.st))
+        self.assertTrue(testtools.followed_all_flags(self, self.story))
 
     def test_has_basic_infos(self):
         data = [
-                ("story", self.st, self.ma.yuko, self.ma.tagawa),
+                ("story", self.story, self.ma.yuko, self.ma.tagawa),
                 ("ep1", self.ep1, self.ma.yuko, self.ma.tagawa),
                 ("ep2", self.ep2, self.ma.yuko, self.ma.tagawa),
                 ("ep3", self.ep3, self.ma.yuko, self.ma.tagawa),
@@ -47,7 +56,7 @@ class StoryTest(unittest.TestCase):
     def test_has_outline_infos(self):
         ma = self.ma
         data = [
-                ("story", self.st,
+                ("story", self.story,
                     ma.yuko.know(ma.tagawa, ma.reason).want(),
                     ma.yuko.look(ma.tagawa, "サボる"),
                     ma.yuko.go(ma.tagawa, ma.tanbo),
@@ -56,16 +65,16 @@ class StoryTest(unittest.TestCase):
                     ma.yuko.know(ma.tagawa, ma.reason).want(),
                     ma.yuko.look(ma.tagawa, "サボる"),
                     ma.yuko.think("サボる決断"),
-                    ma.yuko.go("学校を抜け出す")),
+                    ma.yuko.go(ma.school, "抜け出す")),
                 ("ep2", self.ep2,
                     ma.yuko.look(ma.tagawa, "どこにいるか"),
                     ma.yuko.know(ma.tagawa, ma.reason).want(),
-                    ma.yuko.go(ma.tagawa, "尾行"),
+                    ma.yuko.hear(ma.granpa, ma.tagawa, "居場所"),
                     ma.yuko.know(ma.tagawa, ma.reason)),
                 ("ep3", self.ep3,
-                    ma.yuko.think(ma.myfuture),
+                    ma.yuko.think(ma.myfuture).must(),
                     ma.yuko.hear(ma.tagawa, ma.reason),
-                    ma.yuko.talk(ma.takemura, "相談"),
+                    ma.yuko.talk(ma.tagawa, "将来"),
                     ma.yuko.think(ma.myfuture)),
                 ("ep4", self.ep4,
                     ma.yuko.look(ma.myfuture).want(),
