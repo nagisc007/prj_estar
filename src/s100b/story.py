d0da97df4430c002f64656c3302bf3fd7c0ab4bd
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-04-19 14:15:20 +0900

    [WIP] Added #53: production (draft) (#55)
    * [WIP] Added: production (draft)
    
    * Added: detail and arrange char count
    
    * Fixed: minor detail
    
    * Fixed: 100 chars production
    
    checked list: production

--- src/s100b/story.py
+++ src/s100b/story.py
@@ -47,17 +47,36 @@ def master():
     return m
 
 def story(m: Master):
-    return m.story("はじめましての百文字語り",
-            m.my.be(m.apart, m.day1),
-            m.my.remember(m.you, m.website),
-            m.my.talk(m.you).non(),
-            m.my.deal("伝える", m.you, m.some()).want(),
-            m.my.look(m.estar, m.contest),
-            m.my.know(m.estar),
-            m.my.think("何もない"),
-            m.my.think("気づく", "原稿も真っ白"),
-            m.my.deal(m.phone),
-            m.my.do(m.firstword, "書く"),
+    return m.story("はじめましての百文字",
+            m.day1.explain("外は雨").d("外はしっとりと雨に濡れている").omit(),
+            m.combine(
+                m.my.be(m.apart, m.day1).d("天井を見上げて", "ぼんやりと思い出す"),
+                m.my.remember(m.you, m.website),
+                ),
+            m.combine(
+                m.my.talk(m.you).non().d("もう言葉を交わすことのないあなたは",
+                    "消えてしまったサイトで幽霊のように笑っているだろうか"),
+                m.my.deal("伝える", m.you, m.some()).want(),
+                ),
+            m.my.look(m.estar, m.contest).d("ふと見かけたサイトの小説のコンテスト").omit(),
+            m.my.know(m.estar).d("知らないサイトだが",
+                "そこにはあなたの想いを作品にしてみませんか",
+                "と書かれている").omit(),
+            m.my.think("何もない").d("だが$Sには何もない").omit(),
+            m.my.think("気づく", "原稿も真っ白").d("けれど思うのだ",
+                "原稿用紙も最初は白じゃないか。",
+                "何かしらの言葉を埋めれば",
+                "あなたがどこかで見てくれるかも知れない"
+                ).omit(),
+            m.my.deal(m.phone).d("$Sはスマートフォンを前に指を挙げる").omit(),
+            m.combine(
+                m.my.remember(m.you, "小説好き").d("それでも小説好きなあなたに向け",
+                    "この言葉から書き出そう"),
+                m.my.do(m.firstword, "書く").d("最初に書く言葉は決めてある。",
+                "あなたに一番最初に伝えた言葉だ。",
+                "それは").omit(),
+            ),
+            m.firstword.explain().d("――はじめまして"),
             )
 
 def main(): # pragma: no cover
