f5f576e6b0b285ed5fe3dab8da7f18bc3db4712e
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-05-06 10:40:12 +0900

    Implement #78: lv1yusha project episode 3 (#83)
    * [WIP] Added: episode 3 draft
    
    * [WIP] Added: episode 3
    
    * [WIP] Added: ep3 scene3
    
    * [WIP] Added: ep3 scene4
    
    * Added: ep3 scene5
    
    * Fixed: detail
    
    checked list

--- src/lv1yusha/config.py
+++ src/lv1yusha/config.py
@@ -9,7 +9,7 @@ sys.path.append('storybuilder')
 
 CHARAS = (
         ("hero", "アリエル", 16, "male", "勇者",
-            "me:僕:my:アリエル:dad:父:child_dad:お父さん:mam:母:mother:母さん:bazem:師匠:na_bazem:バーゼム:na_father:オルンガ:anabel:アナ:na_anabel:アナベル:vern:ヴェルン",
+            "me:僕:my:アリエル:dad:父:child_dad:お父さん:mam:母:mother:母さん:bazem:師匠:na_bazem:バーゼム:na_father:オルンガ:anabel:アナ:na_anabel:アナベル:vern:ヴェルン:diana:ディアナ:maririn:マリリン:kult:クルト",
             "英雄の息子"),
         ("hero99", "アリエル９９", 20, "male", "勇者", "me:俺", "レベル99になったアリエル"),
         ("mother", "ウリア", 36, "female", "針子", "me:私:hero:アリエル:bazem:バーゼム"),
@@ -21,16 +21,17 @@ CHARAS = (
         ("anabel_mam", "アナベルの母", 40, "female", "花屋", "me:私:anabel:ベル"),
         ("princess", "エルザ", 16, "female", "王女", "me:わたくし"),
         # chapter1
-        ("diana", "ディアナ", 27, "female", "戦士", "me:アタイ"),
-        ("kult", "クルト", 25, "male", "僧侶", "me:私"),
-        ("maririn", "マリリン", 22, "female", "魔法使い", "me:わたし"),
+        ("diana", "ディアナ", 27, "female", "戦士", "me:アタイ:father:オルンガ:maririn:マリリン:kult:クルト"),
+        ("kult", "クルト", 25, "male", "僧侶", "me:私:maririn:お嬢様:diana:ディアナさん"),
+        ("maririn", "マリリン", 22, "female", "魔法使い", "me:わたし:diana:ディアナ:kult:クルト"),
         ("daemon1", "アークド・ダエモン", 99, "monster", "魔物", "me:オレサマ"),
         ("bazem", "バーゼム", 65, "male", "神官", "me:私:hero:アリエル"),
         ("marc", "マーク", 35, "male", "傭兵", "me:俺"),
         ("monster1", "化けガラス", 99, "monster", "魔物"),
         ("gandof", "ガンドフ", 39, "male", "武器屋", "me:俺"),
         ("gatekeeper1", "門番１", 28, "male", "兵士（門番）", "me:私"),
-        ("barmaster", "酒場の主", 55, "male", "酒場の店主", "me:私"),
+        ("barmaster", "イルダ", 55, "female", "酒場の店主", "me:私:diana:ディアナ"),
+        ("muronof", "ムロノフ", 52, "male", "金物屋の店主", "me:オレ:barmaster:イルダ:mother:ウリア"),
         )
 
 
@@ -40,7 +41,7 @@ STAGES = (
         ("town", "アルアバン城下町"),
         ("myhome", "勇者の家"),
         ("church", "教会"),
-        ("bar", "酒場"),
+        ("bar", "イルダの酒場"),
         ("field1", "アルアバン周辺"),
         ("tower1", "次元の塔"),
         ("minan", "ミナン", "港町"),
@@ -57,6 +58,9 @@ DAYS = (
 
 ITEMS = (
         ("brokens", "折れた剣", "父の形見"),
+        # chapter 1
+        ("kingmark", "薔薇剣の紋章"),
+        ("rosensword", "薔薇剣"),
         )
 
 
@@ -83,6 +87,7 @@ INFOS = (
         ("trial_tower", "試練の塔"),
         ("gameworld", "ゲーム世界"),
         ("bug", "バグ"),
+        # chapter 1
         )
 
 
