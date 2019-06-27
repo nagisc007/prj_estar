66dae3009bcc0ca45c77f570d44bf0e63040b407
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-06-27 21:15:36 +0900

    Added #192: work episode in lost-book project (#206)
    * Added: scene1
    
    * Added: work scene
    
    * [WIP] Added: working scene
    
    * Added: last scene

--- src/lostbook/config.py
+++ src/lostbook/config.py
@@ -10,20 +10,21 @@ sys.path.append('storybuilder')
 # configs
 CHARAS = (
         # main
-        ("nao", "朝川,那緒", 18, "male", "高校生", "me:僕:akiko:明子さん:anzu:高田さん:tamura:田村"),
+        ("nao", "朝川,那緒", 18, "male", "高校生", "me:僕:akiko:明子さん:anzu:高田さん:tamura:田村:sanae:奥貫さん"),
         ("saya", "山本,沙夜", 28, "female", "作家", "me:わたし"),
         ("akiko", "葛城,明子", 68, "female", "本屋", "me:私:nao:朝川君"),
         # sub
         ("anzu", "高田,杏", 18, "female", "高校生", "me:わたし:nao:朝川"),
         ("tamura", "田村,典行", 18, "male", "高校生", "me:オレ:nao:朝川:anzu:杏"),
-        ("akikosun", "葛城,息子", 30, "male", "会社員", "me:ぼく"),
+        ("ak_sun", "葛城,息子", 30, "male", "会社員", "me:ぼく"),
         ("yamamoto", "山本,真夜", 68, "female", "元教員", "me:私"),
         # family
-        ("dad", "朝川,父", 48, "male", "会社員", "me:俺:nao:お前"),
-        ("mam", "朝川,母", 47, "female", "事務員", "me:私:nao:あんた"),
+        ("dad", "朝川,父", 48, "male", "会社員", "me:俺:nao:那緒:y_nao:お前"),
+        ("mam", "朝川,母", 47, "female", "事務員", "me:私:nao:那緒:y_nao:あんた"),
         # school
         ("teacher", "担任", 35, "male", "教師", "me:おれ"),
         # mob
+        ("sanae", "奥貫,早苗", 42, "female", "パート", "me:あたし:nao:朝川君"),
         )
 
 STAGES = (
@@ -36,6 +37,8 @@ STAGES = (
         ("home", "朝川家"),
         ("partfactory", "弁当工場"),
         ("herhome", "葛城家"),
+        # Ride
+        ("bus", "バス"),
         # Part
         ("myroom", "那緒の部屋"),
         ("dyning", "食堂"),
@@ -52,6 +55,7 @@ DAYS = (
         ("current", "現在"),
         ("known", "潰れることを知った日", 7, 12, 2019),
         ("termend1", "一学期の終業式", 7,19, 2019),
+        ("workfirst", "初出勤日", 7,23, 2019),
         # sub
         )
 
