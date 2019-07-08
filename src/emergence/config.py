990adcc6a69d00112ad22b9064a0e7299d987e4c
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-07-08 15:24:38 +0900

    [WIP] Added #224: scene (#226)
    * [WIP] Added: scene
    
    * Added: episode test
    
    * [WIP] Added: setting and first scene
    
    * [WIP] Added: outline scene2
    
    * [WIP] Added: in her room
    
    * Added: scene detail
    
    * Fixed: forget to add scenes

--- src/emergence/config.py
+++ src/emergence/config.py
@@ -10,11 +10,17 @@ sys.path.append('storybuilder')
 # configs
 CHARAS = (
         # main
-        ("miyako", "斎賀,美耶古", 16, "female", "高校生", "me:わたし"),
+        ("miyako", "斎賀,美耶古", 16, "female", "高校生", "me:わたし:aya:彩"),
         ("kirimura", "霧村,豊", 35, "male", "教師", "me:俺"),
         # sub
         ("aya", "千覧,彩", 16, "female", "高校生", "me:私"),
         ("kikyo", "千覧,桔梗", 32, "female", "教祖", "me:私"),
+        # family
+        ("mam", "斎賀,母", 40, "female", "会社員", "me:私"),
+        ("dad", "斎賀,父", 45, "male", "会社員", "me:僕"),
+        ("sister", "斎賀,伽耶", 14, "female", "中学生", "me:ワタシ"),
+        # mob
+        ("nogi", "野木,若葉", 40, "female", "信者", "me:私"),
         )
 
 STAGES = (
@@ -22,12 +28,20 @@ STAGES = (
         ("Shizuoka", "静岡県"),
         # Place
         ("highschool", "県立高校"),
+        ("myhome", "斎賀家"),
+        ("ayahome", "千覧家"),
         # Part
+        ("myroom", "自室"),
         ("ayaroom", "親友の部屋"),
+        ("classroom", "教室"),
+        ("artroom", "美術室"),
         )
 
 DAYS = (
         # main
+        ("bloodmoon", "皆既月食日", 1,21, 2019),
+        ("vanishnext", "消えた翌日", 1,22, 2019),
+        ("valentain", "バレンタイン", 2,14, 2019),
         ("discover1", "発見日", 7,5, 2019),
         # sub
         )
@@ -35,12 +49,17 @@ DAYS = (
 ITEMS = (
         # main
         ("sanagi", "蛹"),
+        ("aya_letter", "彼女の手紙"),
+        ("hermessage1", "※美耶古へのメッセージ"),# TODO: メッセ内容
+        ("spider", "※蜘蛛の名前"),# TODO:妖魔名
         # sub
         )
 
 INFOS = (
         # theme
         # main
+        ("rescue_aya", "彼女を助ける"),
+        ("rebirth", "生まれ変わり"),
         ("ijime", "虐め問題"),
         ("aya_mind", "彼女の気持ち"),
         # sub
--- src/emergence/story.py
+++ src/emergence/story.py
@@ -13,30 +13,162 @@ THM = cnf.THEMES
 
 # scenes
 def sc_redmoon(w: wd.World):
-    miyako = w.miyako
+    miyako, aya = w.miyako, w.aya
     return w.scene("赤い月",
-            miyako.look(w.info("赤い月"),"赤い月を見ていた"),
+            miyako.be(w.stage.myroom),
+            miyako.look("赤い月を見ていた"),
+            aya.talk("ブラッドムーンて言うんだ", "皆既月食"),
+            miyako.deal("その言葉を最後に返信がなくなった"),
+            )
+
+def sc_hermother(w: wd.World):
+    miyako, kikyo = w.miyako, w.kikyo
+    return w.scene("彼女の母",
+            miyako.go(w.stage.ayahome, w.day.vanishnext),
+            kikyo.talk("ごめんなさい。", "あの子部屋から出てこなくて"),
+            miyako.reply("わかりました"),
+            miyako.think("彼女の母はちょっと変わった人で苦手だった"),
+            miyako.look("知らない女性が入れ替わりに入って行った"),
+            miyako.think("けれど翌日も次の日もずっと",
+                "$ayaとは連絡がつかないままだった"),
+                w.miyako.look(w.aya, w.i.herstatus),
+                w.miyako.think(w.aya, w.i.herrefusal),
+            )
+
+def sc_boringday(w: wd.World):
+    miyako, kiri = w.miyako, w.kirimura
+    return w.scene("退屈な日々",
+            miyako.be(w.stage.classroom, w.day.valentain),
+            miyako.hear("先生の言葉がどんどんすりぬけていく"),
+            miyako.think("あれからずっと$ayaと連絡が取れない。",
+                "家にも入れてもらえない。",
+                "ただＬＩＮＥの既読だけが細い糸のような繋がりだった"),
+            # TODO: ayaの学校での状況、いじめ、孤立がち
+            miyako.remember("そういえば$ayaが美術部だったことを思い出す"),
+            )
+
+def sc_strangeman(w: wd.World):
+    miyako, kiri = w.miyako, w.kirimura
+    return w.scene("妙な先生",
+            miyako.come(w.stage.artroom),
+            miyako.look("先生の絵を見た"),
+            miyako.look("あまりの迫力に気圧された"),
+            # TODO: ayaの執念、悲しみ、そんなものが表現された
+            kiri.talk("たった一人の部員がいなくなってしまってな"),
+            miyako.ask("$ayaのことですか？"),
+            kiri.ask("友だちなのか？"),
+            miyako.think("友だち", "なんだろうか"),
+            )
+
+def sc_transformed(w: wd.World):
+    miyako, kiri, kikyo = w.miyako, w.kirimura, w.kikyo
+    return w.scene("彼女の変身",
+            miyako.go(w.stage.ayahome),
+            kiri.ask("すみません。", "$ln_kirimuraですが"),
+            miyako.look("不在のようだった"),
+            kiri.talk("開くぞ……"),
+            miyako.look("初めて入った$ayaの家の中",
+                "妙な置物や絵が飾られていて不気味だ"),
+            miyako.move("階段を上がる"),
+            w.miyako.come(w.stage.ayaroom, w.day.discover1),
+            miyako.move("返事がない。",
+                "先生がドアを開けた"),
+            miyako.look("そこはカーテンが閉め切られて真っ暗な中",
+                "不自然なほどの埃が舞い上がってうっすら霧掛かっているようにも見えた"),
+            miyako.talk("$aya？"),
+            miyako.look("先生が電気をつける。",
+                "浮かび上がったそれに思わず口を押さえた"),
+            # TODO: 蛹描写
+            miyako.deal("それは$ayaからのＬＩＮＥだった"),
+            miyako.look("$hermessage1"),
+            w.miyako.look(w.aya, w.sanagi),
+                w.miyako.look(w.kirimura, w.i.hercause),
+                w.miyako.think(w.i.rescue_aya),
+                w.miyako.meet(w.aya),
+                w.miyako.know(w.i.ijime),
+                w.miyako.go(w.kikyo),
+            miyako.hear("先生が救急車を呼びつける声を聞きながら",
+                "$meはその巨大な繭から伸びた一本の赤い糸を見つけた"),
+            miyako.talk("糸？"),
+            )
+
+def sc_othergirls(w: wd.World):
+    miyako, kiri = w.miyako, w.kirimura
+    return w.scene("蛹の少女たち",
+            miyako.think("その後",
+                "学校で不登校だった生徒が一斉に調べられ",
+                "この学校だけで五名の生徒が繭化しているのが発見された"),
+            miyako.come(w.stage.artroom),
+            miyako.ask("あの", "先生？"),
+            kiri.talk("これ見てみろ"),
+            miyako.look("そこにあったのは先日まで$ayaが描いていたキャンバスだった"),
+            miyako.look("今はそこに赤く咲いた少女たちが描かれていた"),
+            kiri.talk("妙だと思って剥がしたらこれが出てきた"),
+            miyako.ask("あの", "実は"),
+            miyako.think("$meはずっと参加を促されていたＬＩＮＥグループの話をした"),
+            kiri.talk("ちょっと預からせてくれないか？"),
+            )
+
+def sc_catchmyheart(w: wd.World):
+    miyako = w.miyako
+    mam, dad, sis = w.mam, w.dad, w.sister
+    return w.scene("囚われる心",
+            miyako.be(w.stage.myhome),
+            # TODO: 家族の食卓、野木さんの母がおかしい、宗教？
+            )
+
+def sc_crazy(w: wd.World):
+    miyako = w.miyako
+    return w.scene("狂った女",
+            # TODO: 壊れている野木、桔梗の話
+            )
+
+def sc_rebirth(w: wd.World):
+    miyako, kikyo = w.miyako, w.kikyo
+    return w.scene("生まれ変わる",
+            )
+
+def sc_mymind(w: wd.World):
+    miyako, kikyo, kiri, aya = w.miyako, w.kikyo, w.kirimura, w.aya
+    return w.scene("私も蛹に",
+                w.miyako.know(w.i.aya_mind),
+                w.miyako.meet(w.kikyo),
+                w.miyako.look(w.aya_letter),
+                w.aya.do(w.i.rebirth),
+            miyako.look(aya, "真っ赤になって生まれでた彼女の姿だった"),
+            miyako.talk("ごめん。", "$me何も分かってなかった"),
+            kiri.talk("$ln_miyako！"),
+            miyako.look("先生が引っ張り出してくれる"),
+            miyako.talk("先生！", "$ayaが！"),
+            miyako.look("赤に溶けていく。",
+                "そこから一斉に真っ赤な蝶が飛び立った"),
+            kiri.talk("こいつだ"),
+            miyako.look("先生が見せてくれたのは小さくて真っ白な蜘蛛だった"),
+            kiri.talk("$spider。",
+                "人の悲しみを喰らう妖魔だ"),
             )
 
 # episodes
 def ep_intro(w: wd.World):
     return (w.chaptertitle("冒頭"),
             sc_redmoon(w),
-            w.miyako.think(w.aya, w.info("見てない")),
-                w.miyako.look(w.aya, w.i.herstatus),
-                w.miyako.think(w.aya, w.i.herrefusal),
             )
 
 def ep_chrysalis(w: wd.World):
     return (w.chaptertitle("蛹の少女たち"),
-            w.miyako.come(w.stage.ayaroom, w.day.discover1),
-            w.miyako.look(w.aya, w.sanagi),
-                w.miyako.look(w.kirimura, w.i.hercause),
+            sc_hermother(w),
+            sc_boringday(w),
+            sc_strangeman(w),
+            sc_transformed(w),
             )
 
 def ep_reasons(w: wd.World):
     return (w.chaptertitle("彼女たちの理由"),
-                w.miyako.know(w.i.aya_mind),
+            sc_othergirls(w),
+            sc_catchmyheart(w),
+            sc_crazy(w),
+            sc_rebirth(w),
+            sc_mymind(w),
             )
 
 # outline
@@ -52,6 +184,18 @@ def story_outlines(w: wd.World):
                 w.miyako.think(w.aya, w.i.herrefusal),
                 w.miyako.look(w.kirimura, w.i.hercause),
                 w.miyako.know(w.i.aya_mind),
+                True),
+            ("ep1", ep_chrysalis(w),
+                w.miyako.think(w.i.rescue_aya),
+                w.miyako.meet(w.aya),
+                w.miyako.know(w.i.ijime),
+                w.miyako.go(w.kikyo),
+                True),
+            ("ep2", ep_reasons(w),
+                w.miyako.know(w.i.aya_mind),
+                w.miyako.meet(w.kikyo),
+                w.miyako.look(w.aya_letter),
+                w.aya.do(w.i.rebirth),
                 True),
             ]
 
