3f5817f9569c9fc6c5ebe48c33256f8d377df455
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-07-13 09:35:04 +0900

    Added #229: 1st scene (#230)
    * Added: 1st scene
    
    * Added: first scene
    
    * Added: new scene - her drawing
    
    * Fixed: tag
    
    * [WIP] Added: look her drawing
    
    * Added: talking kirimura
    
    * Added: new scene - her home
    
    * [WIP] Added: new scene her room
    
    * Added: trasnformed
    
    * [WIP] Added: aya and miyako
    
    * Added: last scene

--- src/emergence/config.py
+++ src/emergence/config.py
@@ -10,10 +10,10 @@ sys.path.append('storybuilder')
 # configs
 CHARAS = (
         # main
-        ("miyako", "斎賀,美耶古", 16, "female", "高校生", "me:わたし:aya:彩"),
-        ("kirimura", "霧村,豊", 35, "male", "教師", "me:俺"),
+        ("miyako", "斎賀,美耶古", 16, "female", "高校生", "me:わたし:aya:彩:S:美耶古:kirimura:霧村"),
+        ("kirimura", "霧村,豊", 35, "male", "教師", "me:俺:miyako:斎賀:aya:千覧"),
         # sub
-        ("aya", "千覧,彩", 16, "female", "高校生", "me:私"),
+        ("aya", "千覧,彩", 16, "female", "高校生", "me:私:miyako:美耶古"),
         ("kikyo", "千覧,桔梗", 32, "female", "教祖", "me:私"),
         # family
         ("mam", "斎賀,母", 40, "female", "会社員", "me:私"),
@@ -26,8 +26,9 @@ CHARAS = (
 STAGES = (
         # Area
         ("Shizuoka", "静岡県"),
+        ("Town", "牧之川市"),
         # Place
-        ("highschool", "県立高校"),
+        ("highschool", "牧之川高校"),
         ("myhome", "斎賀家"),
         ("ayahome", "千覧家"),
         # Part
@@ -42,6 +43,7 @@ DAYS = (
         ("bloodmoon", "皆既月食日", 1,21, 2019),
         ("vanishnext", "消えた翌日", 1,22, 2019),
         ("valentain", "バレンタイン", 2,14, 2019),
+        ("springbreak", "春休み", 3,16, 2019),
         ("discover1", "発見日", 7,5, 2019),
         # sub
         )
@@ -52,6 +54,7 @@ ITEMS = (
         ("aya_letter", "彼女の手紙"),
         ("hermessage1", "※美耶古へのメッセージ"),# TODO: メッセ内容
         ("spider", "※蜘蛛の名前"),# TODO:妖魔名
+        ("religion", "神月教"),
         # sub
         )
 
