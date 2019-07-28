8a7af092e549a8120ba9f9a5b5185352d139faf4
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-07-28 21:49:46 +0900

    Draft dislike project (#245)
    * Added: some scene and first scene
    
    * [WIP] Added: morning
    
    * [WIP] Added: goto husband work
    
    * [WIP] Added: husband message
    
    * [WIP] Added: think about her
    
    * [WIP] Added: todo
    
    * Added: shopping
    
    * [WIP] Added: brian visit
    
    * Added: bot scene
    
    * Added: recoding my mind
    
    * Added: last scene
    
    * Fixed: dtail
    
    * Fixed: last part

--- src/disliked/config.py
+++ src/disliked/config.py
@@ -10,12 +10,13 @@ sys.path.append('storybuilder')
 # configs
 CHARAS = (
         # main
-        ("satomi", "大邑,聡美", 33, "female", "会社員", "me:私"),
-        ("bot", "夫ボット", 99, "male", "緩和ボット", "me:ボク"),
+        ("satomi", "大邑,聡美", 33, "female", "会社員", "me:私:ayano:文乃"),
+        ("bot", "夫ボット", 99, "male", "緩和ボット", "me:ボク:satomi:聡美:ayano:文乃"),
         # sub
         ("taka", "大邑,隆文", 32, "male", "研究者", "me:僕"),
-        ("brian", "田中,ブライアン", 35, "male", "研究者", "me:オレ"),
+        ("brian", "田中,ブライアン", 35, "male", "研究者", "me:オレ:taka:隆文"),
         # family
+        ("ayano", "大邑,文乃", 5, "female", "子供", "me:あや:satomi:ママ:taka:パパ:bot:ターくん"),
         # mob
         )
 
@@ -25,6 +26,7 @@ STAGES = (
         ("Tamachi", "田町"),
         # Place
         ("myhome", "大邑家"),
+        ("geosys", "ジオシステムズ"),
         # Part
         ("living", "リビング"),
         )
@@ -42,6 +44,9 @@ ITEMS = (
         ("botname", "ローレル"),
         ("botna_eng", "ＬｏＲｅｌ"),
         ("botna_long", "Lost Relieved bot"),
+        ("suki", "嫌い"),
+        ("kirai", "好き"),
+        ("AI", "エキスパート型人工知能"),
         # sub
         )
 
