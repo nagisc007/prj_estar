89b2bb6f3c0e1e1f26d14fe33cdd365d0c2fc98a
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-08-07 09:19:06 +0900

    Added #254: base episode in festa project (#257)
    * Added: base episode
    
    * Fixed: typo
    
    * [WIP] Added: scene

--- src/festa/config.py
+++ src/festa/config.py
@@ -10,14 +10,24 @@ sys.path.append('storybuilder')
 # configs
 CHARAS = (
         # main
+        ("hikawa", "氷川,夢美", 27, "female", "教師", "me:私:sudo:須藤君"),
+        ("takao", "高雄,健輔", 25, "male", "かき氷売り", "me:俺"),
+        ("sudo", "須藤,正嗣", 17, "male", "高校生", "me:僕"),
         # sub
+        ("kirimura", "霧村,豊", 35, "male", "教師", "me:俺:hikawa:氷川:miyako:斎賀:aya:千覧"),
         # mob
         )
 
 STAGES = (
         # Area
+        ("Nagano", "長野県"),
+        ("hometown", "瀬能市"),
         # Place
+        ("shrine", "神社"),
+        ("hightschool", "高校"),
         # Part
+        ("classroom", "教室"),
+        ("staffroom", "職員室"),
         )
 
 DAYS = (
--- src/festa/story.py
+++ src/festa/story.py
@@ -12,8 +12,88 @@ THM = cnf.THEMES
 
 
 # scenes
+def sc_nightfesta(w: wd.World):
+    h = hikawa = w.hikawa
+    sudo, takao = w.sudo, w.takao
+    return w.scene("夜祭り",
+            w.tag.comment("夢美の視点の三人称。最後に彼女は向こう側にいってしまうので"),
+            h.feel("甘いシロップの匂いに鉄板の上で焼けるソースのそれが混ざり合って",
+                "$n_hikawaはいつもふらつきを覚えてそれに気づく"),
+            hikawa.talk("あ、ごめんなさい"),
+            sudo.talk("大丈夫？　石畳がところどころ浮いてるから気をつけないとね"),
+            h.deal("紺の浴衣姿の$n_sudoが野球部らしい丸刈りの頭で手を伸ばす。それを取って立ち上がると改めて周囲を見渡した"),
+            h.look("夏の一日だけ行われる夜祭り。神社の境内に沢山の夜店が並び、人が溢れる。誰もが浴衣だ。同級生も多く、制服姿のままで着ている人は上級生だろうか。誰もがそれぞれの恋を探してキラキラとした表情をしている"),
+            takao.talk("お、今年も来たね。どうだい、彼氏と食べていく？"),
+            h.deal("そう声を掛けたのはかき氷マシンを手動で必死に回している$n_takaoだ。近所で小さい頃からよく遊んでもらっていた"),
+            hikawa.talk("彼氏とかじゃないですよ"),
+            sudo.talk("あ、どうも。須藤といいます。いつもマネージャーでお世話してもらってます"),
+            takao.talk("おお野球部員か。後輩よ。最近どうだ？　$meらの頃は弱かったが最近は県大会いいとこまでいけるか？"),
+            sudo.talk("全然ですよ"),
+            hikawa.talk("あの……いいですか"),
+            h.deal("彼女はいつも決まってストロベリーを注文する。何でも王道を選んでしまうのだ。冒険できないのがたまに悲しいと友だちには漏らしていた"),
+            sudo.talk("それじゃあ$meも"),
+            takao.talk("よし、二つだな。特別サービスしてやるよ"),
+            h.deal("二人ともが山盛りになったかき氷を手に、少し離れたところにあるベンチに向かった"),
+            h.deal("けどそこは既に満席で、二人して苦笑すると、手に持ったまま食べながら歩く"),
+            h.move("長い階段の途中で休み、ちょっと食べ損ねた分をどうしようか迷っていたら、$sudoが「いいよ」ともらってくれた"),
+            h.feel("冷えているはずなのに温かい。そんな気持ちをどう伝えればいいだろう"),
+            h.look("不意に提灯が消え", "寒風が抜ける"),
+            )
+
+def sc_mydream(w: wd.World):
+    h = hikawa = w.hikawa
+    return w.scene("いつもの夢",
+            h.look("目覚めるとじっとりと首筋が濡れていた。触るとひやりとする"),
+            h.deal("時計を確認するとまだ三時半だ"),
+            h.look("カーテンを引いて外を見る。遠くで奇妙な獣の声が響いていた"),
+            # TODO: 簡単な彼女の現状の気持ち紹介（既婚者。隣に須藤の寝顔）
+            )
+
+def sc_schoollife(w: wd.World):
+    h = hikawa = w.hikawa
+    return w.scene("私の人生",
+            # TODO: 学校での私生活、職員室で霧村と
+            )
+
+def sc_ghostdream(w: wd.World):
+    h = hikawa = w.hikawa
+    return w.scene("幽霊の夢",
+            # TODO: また夜祭り、そこで繰り返しの悲劇、やり直したい何か
+            )
+
+def sc_snowfesta(w: wd.World):
+    h = hikawa = w.hikawa
+    return w.scene("雪の夜祭り",
+            # TODO: 夜祭りの中で季節だけが推移していく、それは彼女の生命力
+            )
+
+def sc_truth(w: wd.World):
+    h = hikawa = w.hikawa
+    return w.scene("真実",
+            # TODO: 廃墟神社に行くと、そこで桜が咲いている、だがそれが妖魔。燃やしてしまう
+            )
 
 # episodes
+def ep_intro(w: wd.World):
+    return (w.chaptertitle("夜祭り"),
+            sc_nightfesta(w),
+            sc_mydream(w),
+            # NOTE: 普通の夜祭の様子、主人公、その後悔
+            )
+
+def ep_tragedy(w: wd.World):
+    return (w.chaptertitle("繰り返す悲劇"),
+            sc_schoollife(w),
+            # NOTE: 主人公現在（高校教師）、疲れている、気づく男
+            )
+
+def ep_hermind(w: wd.World):
+    return (w.chaptertitle("祭りの跡"),
+            sc_ghostdream(w),
+            sc_snowfesta(w),
+            sc_truth(w),
+            # NOTE: 真実暴露、主人公の選ぶ「現在」
+            )
 
 # outline
 def story_baseinfos(w: wd.World):
@@ -33,6 +113,9 @@ def world():
 
 def story(w: wd.World):
     return (w.maintitle("夜祭り"),
+            ep_intro(w),
+            ep_tragedy(w),
+            ep_hermind(w),
             )
 
 
