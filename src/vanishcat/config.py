34424ad1f30288155a92fd14a7ba6a861394fa6f
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-09-13 08:31:00 +0900

    [WIP] Added: first part (#271)
    * [WIP] Added: first part
    
    * Added: first scene
    
    * [WIP] Added: back home and lost
    
    * [WIP] Added: look for cat
    
    * [WIP] Added: goto park
    
    * Added: catch her
    
    * [WIP] Added: back home cat
    
    * [WIP] Added: vanish everyday
    
    * Added: call kieth
    
    * [WIP] Added: talk truth
    
    * [WIP] Added: last part
    
    * Added: last

--- src/vanishcat/config.py
+++ src/vanishcat/config.py
@@ -10,9 +10,13 @@ sys.path.append('storybuilder')
 # configs
 CHARAS = (
         # main
-        ("kisaragi", "如月,祐司", 30, "male", "SE", "me:私"),
+        ("kisaragi", "如月,祐司", 30, "male", "SE", "me:僕:S:僕:ary:アリィ:arisa:有紗:kieth:キースi:kase:加瀬:aisaka:逢坂"),
         # sub
-        ("irene", "アイリーン", 10, "female", "猫", "me:私"),
+        ("ary", "アリィ", 10, "female", "猫", "me:私:kisa:祐司"),
+        ("arisa", "日陰,有紗", 28, "female", "会社員", "me:私"),
+        ("kieth", "ベーカー,キース", 36, "male", "研究者", "me:オレ:kisa:ユージ"),
+        ("aisaka", "逢坂,豊", 40, "male", "チーフ", "me:私:kisa:如月君"),
+        ("kase", "加瀬,周伍", 28, "male", "SE", "me:俺"),
         # mob
         )
 
@@ -21,6 +25,9 @@ STAGES = (
         ("Atami", "熱海市"),
         # Place
         ("myhome", "自宅"),
+        ("office", "会社"),
+        ("park1", "葛西臨海公園"),
+        ("ramen", "ラーメン店"),
         # Part
         ("living", "リビング"),
         )
@@ -31,6 +38,11 @@ DAYS = (
         ("vanishold", "アイリーンの消えた日"),
         ("deadold", "アイリーンの亡くなった日"),
         ("first", "最初の日"),
+        ("working", "仕事で忙しくする日"),
+        ("vanish1", "最初の失踪日"),
+        ("revive1", "蘇った日１"),
+        ("vanish2", "複数回失踪後のある日"),
+        ("visitjap", "キース来日した日"),
         # sub
         )
 
