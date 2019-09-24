f8761f496f27f51373179b01a452946e8212b0e0
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-09-24 19:01:28 +0900

    [WIP] Added: first scene (#275)
    * [WIP] Added: first scene
    
    * [WIP] Added: struct scene
    
    * [WIP] Added: todo
    
    * [WIP] Added: persons and scene
    
    * Added: last scene

--- src/moonlight/config.py
+++ src/moonlight/config.py
@@ -10,28 +10,42 @@ sys.path.append('storybuilder')
 # configs
 CHARAS = (
         # main
-        ("goro", "皆井,吾朗", 41, "male", "会社員", "me:俺"),
-        ("luna", "皆井,瑠那", 35, "female", "ハープ奏者", "me:私"),
+        ("goro", "皆井,吾朗", 41, "male", "会社員", "me:俺:luna:瑠那:takahashi:高橋:tsukada:塚田さん"),
+        ("luna", "皆井,瑠那", 35, "female", "ハープ奏者", "me:私:吾朗さん"),
         # sub
+        ("takahashi", "高橋,優太", 32, "male", "ヴァイオリニスト", "me:僕:luna:瑠那さん"),
+        ("tsukada", "塚田,善司", 51, "male", "会社員", "me:オレ:goro:皆井"),
         # mob
         )
 
 STAGES = (
         # Area
+        ("Tokyo", "東京都"),
+        ("Meguro", "目黒区"),
         # Place
         ("myhome", "皆井家"),
+        ("company", "吾朗の勤務する会社"),
         # Part
+        ("living", "リビング"),
+        ("balcony", "ベランダ"),
+        ("onstreet", "路上"),
+        ("intrain", "電車内"),
+        ("office", "会社オフィス"),
         )
 
 DAYS = (
         # main
+        ("firstday", "最初の帰宅日"),
+        ("goout", "仕事にでかける日"),
         ("current", "現在"),
+        ("fight", "喧嘩した日"),
         # sub
         )
 
 ITEMS = (
         # main
         # sub
+        ("helpmark", "ヘルプマーク"),# NOTE:赤字に白で＋と♡
         )
 
 INFOS = (
--- src/moonlight/story.py
+++ src/moonlight/story.py
@@ -17,6 +17,9 @@ def sc_darkhome(w: wd.World):
     luna = w.luna
     return w.scene("真っ暗な家の中で",
             h.be(w.stage.myhome, w.day.current),
+            h.look("駅前の喧騒をやり過ごして暗がりの住宅街へと戻ってくる"),
+            h.look("見上げると三日月。暗い中を歩いて帰ると家に近づいて、ハープの音が聞こえる。彼女と分かる"),
+            h.deal("鍵を開けて中に入る。家は真っ暗。そこに音だけが響く"),
             luna.talk("おかえり"),
             # NOTE: 真っ暗な家に戻ってきて、ハープだけが聞こえる、人物紹介兼ねて
             )
@@ -25,14 +28,39 @@ def sc_alwayshelp(w: wd.World):
     h = goro = w.goro
     luna = w.luna
     return w.scene("いつでも助けたい",
+            h.be(w.stage.myhome, w.day.goout),
+            h.deal("朝食の準備をする。彼女は目が見えない。音だけを頼りに生きている"),
+            h.deal("それでも時々見えているんじゃないかと疑ってしまう。そういう所作"),
+            luna.talk("今日は、遅い？"),
+            h.deal("感が鋭い。だからなるべく心配をかけないようにと振る舞う"),
+            goro.talk("いってくるよ。なるべく早く帰らせてもらう"),
             # NOTE: 仕事にでかけるいつもの朝の様子、不安症の夫
+            h.be(w.stage.onstreet),
+            h.deal("外を歩く。駅前。ヘルプマークをつけている女性を見つけ、助ける"),
+            h.think("彼女との出会いもこんなきっかけだったことを思い出す"),
             # NOTE: 会社に行く途中でヘルプマークの人を助ける夫
+            h.be(w.stage.intrain),
+            h.think("吊革を握りながら、彼女が心配"),
+            h.hear("耳障りなシャカシャカ音が大きく"),
+            h.feel("電車が止まり、停電？　ちかちかっと"),
+            h.deal("車内アナウンスで「点検中です。しばらくお待ち下さい」"),
+            h.deal("ほどなくして動き出す"),
             )
 
 def sc_alonegoout(w: wd.World):
     h = goro = w.goro
     luna = w.luna
     return w.scene("孤独な外出",
+            h.be(w.stage.myhome),
+            h.deal("夜遅くに帰宅。家は真っ暗"),
+            h.deal("物音がしない。返事もない"),
+            h.deal("一人でカップ麺を食べていると彼女が起きてきて"),
+            h.deal("明日からの公演に備えて早めに休んだ方がいい。どうした？"),
+            luna.talk("あなたには$meの助けが必要ないんだと思って。おやすみなさい"),
+            h.think("考え込む"),
+            h.deal("翌朝、彼女の楽団のメンバーと挨拶を交わしてお願いする"),
+            h.deal("見送る"),
+            h.deal("その夜、LINEがくる。月が見える？と"),
             # NOTE: 彼女が公演で不在、一人孤独、LINEで「月が見える？」と
             )
 
@@ -40,6 +68,15 @@ def sc_fight(w: wd.World):
     h = goro = w.goro
     luna = w.luna
     return w.scene("二人の喧嘩",
+            h.be(w.stage.myhome, w.day.fight),
+            h.deal("ある日のこと、彼女が突然ハープを弾くのをやめてしまった"),
+            h.deal("公演で何かあったみたいだが、あまり話さないから分からない"),
+            goro.talk("もうちょっと言葉にしてくれないと分からない"),
+            h.deal("けれど彼女は「関係ない」とだけ"),
+            h.deal("そのうち口論になり"),
+            luna.talk("目が見えるあなたには分からないわよ！"),
+            h.deal("言ってはいけないことを口にしたんだと、絶望する"),
+            h.deal("彼女が出ていく。追えない夫"),
             # NOTE: 月のこと（自分がいなくても大丈夫な彼女）で口論になり、出ていく
             )
 
@@ -47,6 +84,15 @@ def sc_worldtone(w: wd.World):
     h = goro = w.goro
     luna = w.luna
     return w.scene("音の世界",
+            h.be(w.stage.myhome),
+            h.deal("楽団の知人の家に泊まっていると高橋から連絡がある"),
+            h.deal("電話の向こうで楽しげな声"),
+            h.think("彼女がどんな気持ちか知らない"),
+            h.deal("暗闇の中で過ごす"),
+            h.hear("様々な息吹。音"),
+            h.think("月なんて見えないだろ、という言葉"),
+            h.deal("外に飛び出す"),
+            h.deal("タクシーを拾い、向かう"),
             # NOTE: 出張中、彼女の音が聞こえてくる、音の世界を思い出し、帰る
             )
 
@@ -54,6 +100,19 @@ def sc_lookmoon(w: wd.World):
     h = goro = w.goro
     luna = w.luna
     return w.scene("月を見る、月を聞く",
+            h.be(w.stage.onstreet),
+            h.deal("高橋に連れられ、彼女が待っていた"),
+            h.deal("彼に感謝をして、彼女を受け取る"),
+            h.deal("彼に少し睨まれた気がした"),
+            h.deal("尋ねる。音の世界のこと"),
+            h.deal("彼女からスマホで月の音を聞かされる"),
+            luna.talk("月だけじゃなくずっと遠くの星もね、音を持っているんだって。誰もが音をもっているの。$meはいつもあなたの音を聞いているのよ？"),
+            h.look("見上げる。雲が多くて見えないだろうと言われていたのに雲が千切れた"),
+            h.look("月が見える"),
+            h.deal("彼女に月を聞かせてと言われる"),
+            h.look("月を見る"),
+            h.deal("言葉は出ない。でも彼女はその胸に耳を当てて言う"),
+            luna.talk("月が、聞こえた"),
             # NOTE: 真っ暗な部屋、彼女の音だけ、スマホで月の音を、月を二人で聞く
             )
 
@@ -97,7 +156,7 @@ def world():
 
 
 def story(w: wd.World):
-    return (w.maintitle("月を聴く彼女"),
+    return (w.maintitle("月が、聴こえた"),
             ep_intro(w),
             ep_yourhelp(w),
             ep_hearmoonlight(w),
