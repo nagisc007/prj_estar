e043ea626cbb08380dff738ce835570c34a04278
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-10-10 14:08:29 +0900

    [WIP] Added: first part in letter (#289)
    * [WIP] Added: first part
    
    * [WIP] Added: roast beaf
    
    * Added: intro scene
    
    * [WIP] Added: next todo
    
    * [WIP] Added: consul her
    
    * [WIP] Added: her hand
    
    * [WIP] Added: working
    
    * [WIP] Added: consul chief
    
    * Added: scene (yoko talk)
    
    * [WIP] Added: letter bot
    
    * Added: modeling
    
    * Added: her dead
    
    * [WIP] Added: birth son
    
    * Added: last part

--- src/letter/config.py
+++ src/letter/config.py
@@ -10,16 +10,20 @@ sys.path.append('storybuilder')
 # configs
 CHARAS = (
         # main
-        ("hiroki", "平松,博樹", 35, "male", "技師", "me:僕:S:博樹"),
-        ("yoko", "平松,葉子", 34, "female", "SE", "me:私"),
+        ("hiroki", "平松,博樹", 35, "male", "技師", "me:僕:S:博樹:yoko:葉子:mizu:課長:ono:大野さん:myson:博史"),
+        ("yoko", "平松,葉子", 34, "female", "SE", "me:私:hiroki:あなた:hirosan:博樹さん"),
         # sub
-        ("chief", "課長", 45, "male", "上司", "me:俺"),
-        ("sister", "葉子の妹", 30, "female", "保育士", "me:わたし"),
+        ("mizuguchi", "水口,賢介", 45, "male", "課長", "me:俺:hiroki:平松"),
+        ("sister", "田端,亮子", 30, "female", "保育士", "me:わたし:hiroki:博樹さん"),
+        ("tabata", "田端,優平", 34, "male", "会社員", "me:オレ:ryoko:亮子"),
+        ("ta_son", "田端,悠斗", 10, "male", "小学生", "me:ぼく"),
         ("myson", "息子", 10, "male", "子ども", "me:ぼく"),
+        ("ono", "大野,善司", 55, "male", "技師", "me:儂"),
+        # future
         ("hiroshi", "平松,博史", 35, "male", "技師", "me:僕:S:博史"),
         ("helper", "助手", 29, "female", "助手", "me:わたし"),
         # mob
-        ("doctor", "医師", 44, "male", "医師", "me:私"),
+        ("doctor", "西部,浩介", 44, "male", "医師", "me:私"),
         )
 
 STAGES = (
@@ -42,6 +46,8 @@ DAYS = (
         ("current", "現在"),
         ("warming", "幸せだった日々"),
         ("consul1", "診察結果日"),
+        ("work1", "出社して上司に相談した日"),
+        ("talkmodel", "妻にモデリングの話をした日"),
         ("model1", "モデリング初日"),
         ("dead", "彼女が亡くなった日"),
         ("birth", "息子が生まれた日"),
